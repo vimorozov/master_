ceba6a0c513e038cd39c1d4b1771caf411eecaa5
vimorozov <dah.play@gmail.com>
2014-10-21 08:32:20 -0500

    Create mysql_fixes.sql

---
+++ mysql_fixes.sql
@@ -0,0 +1,13 @@
+//Eliminar auras duplicadas
+creature_template_addon and remove the entry 
+28611 aura data.
+
+i know it difficult, but the error's can be 
+fixed in an hour or half an hour and then it will work. 
+
+then go to smartscripts and remove those entry 29836.
+
+
+//Eliminar tablas en un rango de datos
+delete from pool_gameobject where pool_entry BETWEEN 10842 AND 10911; 
+

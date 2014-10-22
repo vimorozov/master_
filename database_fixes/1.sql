590b8724e059aaef80e9487a96cb844113c9d705
vimorozov <dah.play@gmail.com>
2014-10-22 09:05:28 -0500

    Create 1.sql

---
+++ database_fixes/1.sql
@@ -0,0 +1,41 @@
+GameObject (Entry: 126339 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 126340 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 126341 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 126345 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 151951 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 126342 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+GameObject (Entry: 191300 GoType: 8) have data0=2061 but SpellFocus (Id: 2061) not exist.
+Gameobject (Entry: 202357 GoType: 2) have data0=0 but lock (Id: 0) not found.
+Gameobject (Entry: 188124 GoType: 3) have data7=188187 but GO (Entry 188187) have not GAMEOBJECT_TYPE_TRAP (6) type.
+Creature (Entry: 636) lists difficulty 1 mode entry 42698 with `AIName` filled in. `AIName` of difficulty 0 mode creature is always used instead.
+Creature (Entry: 37989) has non-existing faction_A template (0).
+Creature template (Entry: 2614) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 2615) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 21993) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 21996) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 21999) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 22001) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 22002) does not exist but has a record in `creature_template_addon`
+Creature template (Entry: 22467) does not exist but has a record in `creature_template_addon`
+Table `creature` have creature (GUID: 200325 Entry: 28477) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 204537 Entry: 28784) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 204538 Entry: 28784) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215523 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215524 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215525 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215526 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215527 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215528 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215529 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 215530 Entry: 10218) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 219298 Entry: 30226) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220208 Entry: 26841) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220209 Entry: 26841) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220210 Entry: 26841) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220212 Entry: 26841) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220214 Entry: 26841) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220367 Entry: 30541) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220371 Entry: 30541) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220372 Entry: 30541) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220373 Entry: 30482) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.
+Table `creature` have creature (GUID: 220380 Entry: 30482) with `MovementType`=0 (idle) have `spawndist`<>0, set to 0.

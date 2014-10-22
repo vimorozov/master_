9221a8f725a5e75d936b37a27f3729c916c30a2a
vimorozov <dah.play@gmail.com>
2014-10-22 09:17:18 -0500

    Create 2.sql

---
+++ database_fixes/2.sql
@@ -0,0 +1,79 @@
+eature` have creature (Entry: 3174) with equipment_id 102 not found in table `creature_equip_template`, set to no equipment.
+Table `creature` has creature (GUID: 309548) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 309473) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 309460) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 309254) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 309052) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 308984) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 308647) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 308167) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 308115) with non existing creature entry 4, skipped.
+Table `creature` has creature (GUID: 308098) with non existing creature entry 4, skipped.
+Couldn't get creature data for GUIDLow 228522
+Couldn't get creature data for GUIDLow 228523
+Couldn't get creature data for GUIDLow 228525
+Couldn't get creature data for GUIDLow 228526
+Couldn't get creature data for GUIDLow 228593
+Couldn't get creature data for GUIDLow 228594
+Couldn't get creature data for GUIDLow 228595
+Couldn't get creature data for GUIDLow 228596
+Couldn't get creature data for GUIDLow 228597
+Couldn't get creature data for GUIDLow 228598
+Couldn't get creature data for GUIDLow 228599
+Couldn't get creature data for GUIDLow 228600
+Couldn't get creature data for GUIDLow 228601
+Couldn't get creature data for GUIDLow 228602
+Couldn't get creature data for GUIDLow 228603
+Couldn't get creature data for GUIDLow 228604
+Couldn't get creature data for GUIDLow 228605
+Couldn't get creature data for GUIDLow 228606
+Couldn't get creature data for GUIDLow 228607
+Couldn't get creature data for GUIDLow 228608
+Couldn't get creature data for GUIDLow 228609
+Couldn't get creature data for GUIDLow 228612
+Couldn't get creature data for GUIDLow 228616
+Couldn't get creature data for GUIDLow 228619
+Couldn't get creature data for GUIDLow 228625
+Couldn't get creature data for GUIDLow 228626
+Couldn't get creature data for GUIDLow 228629
+Couldn't get creature data for GUIDLow 228630
+Couldn't get creature data for GUIDLow 228632
+Couldn't get creature data for GUIDLow 228633
+Couldn't get creature data for GUIDLow 228635
+Couldn't get creature data for GUIDLow 228636
+Couldn't get creature data for GUIDLow 228637
+Couldn't get creature data for GUIDLow 228639
+Couldn't get creature data for GUIDLow 228640
+Couldn't get creature data for GUIDLow 228641
+Couldn't get creature data for GUIDLow 228642
+Couldn't get creature data for GUIDLow 228643
+Couldn't get creature data for GUIDLow 228644
+Couldn't get creature data for GUIDLow 228645
+Couldn't get creature data for GUIDLow 228647
+Couldn't get creature data for GUIDLow 228648
+Couldn't get creature data for GUIDLow 228649
+Couldn't get creature data for GUIDLow 228650
+Couldn't get creature data for GUIDLow 228651
+Couldn't get creature data for GUIDLow 228652
+Couldn't get creature data for GUIDLow 228653
+Couldn't get creature data for GUIDLow 228654
+Couldn't get creature data for GUIDLow 228655
+Couldn't get creature data for GUIDLow 228656
+Couldn't get creature data for GUIDLow 228657
+Couldn't get creature data for GUIDLow 228659
+Couldn't get creature data for GUIDLow 228661
+Couldn't get creature data for GUIDLow 228663
+Couldn't get creature data for GUIDLow 228665
+Couldn't get creature data for GUIDLow 228666
+Couldn't get creature data for GUIDLow 228667
+Couldn't get creature data for GUIDLow 228668
+Couldn't get creature data for GUIDLow 228669
+Couldn't get creature data for GUIDLow 228670
+Couldn't get creature data for GUIDLow 228671
+Couldn't get creature data for GUIDLow 228674
+Couldn't get creature data for GUIDLow 228675
+Couldn't get creature data for GUIDLow 228676
+Couldn't get creature data for GUIDLow 228679
+Couldn't get creature data for GUIDLow 228681
+Couldn't get creature data for GUIDLow 228683
+Couldn't get creature data for GUIDLow 228686
